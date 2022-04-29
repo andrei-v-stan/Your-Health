93d6c88eb667e10d317cf787a049d1e118bab141
Stan Andrei <andrei.stan@info.uaic.ro>
2022-04-29 03:01:09 +0300

    Update v0

---
+++ js/hide_button.js
@@ -0,0 +1,17 @@
+function Hidden() {
+  var x = document.getElementById("hide");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function Hidden2() {
+  var x = document.getElementById("hide2");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
