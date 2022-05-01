4e230290debc7c48b66d154dcc75c0bfc6b07ae2
Stan Andrei <andrei.stan@info.uaic.ro>
2022-05-01 09:34:12 +0300

    Update v0.2

---
+++ js/show_image.js
@@ -0,0 +1,7 @@
+function picture(){
+var pic = "./media/morse_consumer.png"
+document.getElementById('bigpic').src = pic.replace('10x10', '100x100');
+document.getElementById('bigpic').style.display='block';
+document.getElementById('bigpic').style.class='center';
+
+}
